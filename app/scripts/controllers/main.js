14fd860869fa65c41590c3ba2f4947aa7afea665
Ubuntu <ubuntu@ip-172-31-10-155.ap-southeast-2.compute.internal>
2014-12-19 05:56:06 +0000

    project setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanprojectApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
